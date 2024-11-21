9e71f9ade196d83cebe77cf0d0efe63816b7e048
Gabotw <u202018627@upc.edu.pe>
2024-11-21 00:14:11 -0500

    feat: added terms and conditions.

--- lib/network/api_client.dart
+++ lib/network/api_client.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ApiClient {
-  static const String baseUrl = 'https://35ce-38-172-129-244.ngrok-free.app/';
+  static const String baseUrl = 'https://3ad3-38-172-129-245.ngrok-free.app/';
   String? _token;
 
   void setToken(String token) {
